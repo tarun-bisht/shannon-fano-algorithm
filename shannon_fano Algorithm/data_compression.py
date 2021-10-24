19c2abe8297eb1de6a8fc5924cdac0a73c906fa3
tarun-bisht <itstarunbisht@gmail.com>
2021-10-24 15:15:07 +0530

    code cleanup

--- shannon_fano Algorithm/data_compression.py
+++
@@ -1,6 +0,0 @@
-from compression import shannon_fennon_compression
-data=input("Enter Data to Compress: ")
-compressor=shannon_fennon_compression()
-compressed_data=compressor.compress_data(data)
-for i in compressed_data:
-    print(f"Character-- {i.original}:: Code-- {i.code} :: Probability-- {i.probability}")
